3b542e4c8a1ef56022ef68f336c44c4bfcd12028
maldestor95 <maldestor95@gmail.com>
2016-12-05 23:44:56 +0100

    update - no test done

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ or in any subdirectories.
 
 
 ## Installation
-
+`npm install https://github.com/maldestor95/RecursiveDir.git --save`
 
 ## Usage
 
@@ -17,6 +17,25 @@ recursive.parse(path, function(err,files){
   // Files is an array of filename
   console.log(files);  
 })
-});
 ```
+
+var rr=require('./index');
+console.log(rr.list);
+var print=function(e,d){
+  // console.log(e,d);
+  console.log(rr.filteredList);
+};
+
+var listeee=function(){
+  rr.filter("2015",print);
+  rr.filter(["2015","S1","data"],print);
+  rr.clearfilter();
+  rr.filter(["2016"],print);
+  rr.clearfilter();
+  rr.filter("09",print);
+};
+
+rr.parse('c:\\temp',listeee);
+
+## API
 
--- index.js
+++ index.js
@@ -1,43 +1,84 @@
-var recursive = require('recursive-readdir');
+'use strict'
+/**
+ * Includes files
+ */
 var _=require('underscore');
 var path=require('path');
+var fs=require('fs');
+var recursive=require('recursive-readdir');
 
+/**
+ * RecursiveDir Function
+ * Objective scan recursively through a directory and allow filtering on the list 
+ * to match an array list
+ * @private list : array containing the list of all files found during the search
+ * @private filteredList : array after using @function filter
+ */
+ var RecursiveDir={
 
-var RecursiveDir={
-
+// private
  list:null,
  filteredList:null,
 
- parse:function(path, cb){
- // parse the directory with all the files 
- // and create a table in RecursiveDir.list
- // return null if success or an error
-  recursive(path, function (err, files) {
-    
-    if (err!=null) {
-    console.log(err);
-    cb(JSON.stringify(err));
+/**
+ * parse a folder recursively
+ * @param  {string}   searchpath [directory to look into]
+ * @param  {Function} callback   [callback ]
+ * @return {[type]}              [return an error (null if any) and a array containing the list of files]
+ */
+ parse:function(searchpath ,callback){
+   recursive(searchpath, function (err, files) {
+    if (err) {
+      callback(err);
     }
     else {
       RecursiveDir.list=files;
-      filteredList=files;
-      cb(err,files);
+      RecursiveDir.filteredList=files;
+      return callback(null,files);
     }
     });    
- }, 
-
- filter:function(filter,cb){
- // sort the list 
-  if (RecursiveDir.list!=null){
-    console.log(_.filter(RecursiveDir.list,function(k){ return k.match(filter)!=null}))
-  }
-  cb(null,RecursiveDir.list);
  },
 
- clearfilter:function(cb){
+/**
+ * Cumulative Filtering
+ * Pre-requisite: run a parse function previously to create a list
+ * @param  {String or Array}   filter   [if empty; doesn't reduce the filtered list]
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
+ filter:function(filter,callback){
+ // sort the list 
+  var searchfilter=null;
 
- }
+  // private function to perform the filtering
+  var searchfunction=function(sf,cb){
+    if (RecursiveDir.list!=null){
+      cb (null,
+        _.each(sf,function(data){
+        // console.log( data);
+        RecursiveDir.filteredList=_.filter(RecursiveDir.filteredList,function(k){return k.match(data)!=null});
+      })
+      );
+    }   
+  };
+
+  // main loop; decision taken wether the filter is a string or an array
+  if (_.isString(filter)){
+    searchfilter=[filter];
+    searchfunction(searchfilter,callback);
+  }
+  else if (_.isArray(filter)){
+    searchfilter=filter;
+    searchfunction(searchfilter,callback);
+  }
+ },
+
+/**
+ * Re-init of the filter
+ */
+ clearfilter:function(){
+  RecursiveDir.filteredList=RecursiveDir.list;
+  }
 };
 
-
-module.exports = RecursiveDir
+module.exports=RecursiveDir;
